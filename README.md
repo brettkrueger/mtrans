617a6b293fc83d24327e24f644dca7aba6244659
brett <admin@krux.us>
2016-08-17 02:31:12 -0400

    Initial commit of __init__ + README update

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # mtrans
-Translator API for multiple platforms. Presently includes Bing and Google Translate.
+Translator API for multiple platforms. Presently includes Google Translate.
---
+++ __init__.py
@@ -0,0 +1,10 @@
+class Google():
+	def __init__(self,API):
+		self.API=API
+		self.url="https://www.googleapis.com/language/translate/v2"
+
+	def translate(self,translate,from_lang,to_lang):
+		return self.url+"?key={0}&q={1}&source={2}&target={3}".format(self.API,translate.replace(' ','%20'),from_lang,to_lang)
+
+	def detect(self,translate):
+		return self.url+str("/detect?key={0}&q={1}").format(self.API,translate.replace(' ','+'))
